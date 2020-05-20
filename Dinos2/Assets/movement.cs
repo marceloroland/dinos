b79d5108cb783d2d30bd68f11add50e8197ef0e6
marceloroland <mrbernardino@artit.com.br>
2020-05-20 13:21:20 -0300

    git commit para fazer perge com o felipe

--- Dinos2/Assets/movement.cs
+++ Dinos2/Assets/movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     public float speedH = 2.0f;
-    public float speedV = 2.0f;
+    public float speedV = 0.98f;
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
@@ -14,19 +14,19 @@ public class movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * 0.08f);
+            transform.Translate(Vector3.forward * speedV);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * 0.08f);
+            transform.Translate(Vector3.back * speedV);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * 0.08f);
+            transform.Translate(Vector3.right * speedV);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * 0.08f);
+            transform.Translate(Vector3.left * speedV);
         }
 
         yaw += speedH * Input.GetAxis("Mouse X");
